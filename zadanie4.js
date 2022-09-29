0b34a9c6a8578bbbf7a7eb64907aee246615c5ff
Dorota Dawidowicz <dorotadawidowicz@gmail.com>
2022-09-29 23:13:48 +0200

    ex4

---
+++ zadanie4.js
@@ -0,0 +1,26 @@
+// Stworzyć funkcję, która przyjmuje jako argument tablicę obiektów i zwróci ją posortowaną po wartości libraryId.
+
+const library = [
+  {
+    title: "The Road Ahead",
+    author: "Bill Gates",
+    libraryID: 1254,
+  },
+  {
+    title: "Walter Isaacson",
+    author: "Steve Jobs",
+    libraryID: 4264,
+  },
+  {
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    author: "Suzanne Collins",
+    libraryID: 3245,
+  },
+];
+
+function sortByID(a, b) {
+  return a.libraryID - b.libraryID;
+} //funkcja przypina się do klucza libraryID
+
+library.sort(sortByID); //metoda sort() ustawia rosnąco (funkcja pomocnicza) obiekty w tablicy library sortując je po ID
+console.log(library);
