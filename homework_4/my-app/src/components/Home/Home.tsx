e2f6f3e29ee3e435db1fb3d3f4c51ce3b17472c7
Dorota Dawidowicz <dorotadawidowicz@gmail.com>
2022-11-02 14:17:03 +0100

    ex3 done

--- homework_4/my-app/src/components/Home/Home.tsx
+++ homework_4/my-app/src/components/Home/Home.tsx
@@ -1,3 +1,30 @@
-export const Home = () => {
-  return <div>Hello from Home</div>;
+import { FC, useContext } from "react";
+import { Context } from "../../ContextProvider";
+import { useNavigate } from "react-router-dom";
+import styles from "./Home.module.css";
+
+import { signOut } from "firebase/auth";
+import { auth } from "./../../firebase-config";
+
+export const Home: FC = () => {
+  const navigate = useNavigate();
+  const { username, setUsername } = useContext(Context);
+
+  const onLogout = (): void => {
+    // localStorage.removeItem('user');
+    // localStorage.removeItem('products');
+    setUsername("");
+    signOut(auth);
+    navigate("/signup");
+  };
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.header}>Witaj w naszej aplikacji {username}</p>
+
+      <button className={styles.logoutBtn} type="button" onClick={onLogout}>
+        Wyloguj
+      </button>
+    </div>
+  );
 };
